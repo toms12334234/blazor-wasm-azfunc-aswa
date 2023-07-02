c926079599a1fc1a4be3222dd463081c969eb926
Tom Schenkels <tomsch1kels@gmail.com>
2023-07-02 16:08:04 +0200

    remove problematic property from c# code

--- ApiApp/Models/Metadata.cs
+++ ApiApp/Models/Metadata.cs
@@ -8,9 +8,6 @@ namespace ApiApp.Models
     {
         public virtual Dictionary<string, string> Links { get; set; } = new();
 
-        [JsonProperty("next_page", Required = Required.AllowNull)]
-        public virtual string? NextPage { get; set; }
-
         [JsonProperty("wpcom")]
         public virtual bool IsWordpressCom { get; set; }
     }
