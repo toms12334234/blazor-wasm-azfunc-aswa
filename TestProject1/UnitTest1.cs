cc39d53f0c3d49143d233f40e4ffae6717eb79f5
Tom Schenkels <tomsch1kels@gmail.com>
2023-07-03 22:08:33 +0200

    Add test project

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,12 @@
+using WebApp.PostHttpTrigger;
+namespace TestProject1;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(1,1);
+        PostHttpTrigger.
+    }
+}
